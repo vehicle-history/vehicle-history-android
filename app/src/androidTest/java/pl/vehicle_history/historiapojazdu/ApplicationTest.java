2025183d47da922c68e18e2ba9462d47779a8726
m4lysh <m4lysh@gmail.com>
2015-03-06 10:59:49 +0100

    Initial commit

---
+++ app/src/androidTest/java/pl/vehicle_history/historiapojazdu/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.vehicle_history.historiapojazdu;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
