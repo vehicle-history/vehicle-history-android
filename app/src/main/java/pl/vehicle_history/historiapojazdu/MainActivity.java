341e4dbdcc2de5a9b7411005ef8d2395776606b7
Piotr Makowski <piotr.makowski@allegrogroup.com>
2015-03-14 11:13:00 +0100

    Extract PlaceholderFragment to own file.

--- app/src/main/java/pl/vehicle_history/historiapojazdu/MainActivity.java
+++ app/src/main/java/pl/vehicle_history/historiapojazdu/MainActivity.java
@@ -1,18 +1,13 @@
 package pl.vehicle_history.historiapojazdu;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import pl.vehicle_history.api.method.AuthMethod;
 import pl.vehicle_history.api.method.ResponseListener;
@@ -124,45 +119,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
             }
         });
         auth.makeRequest();
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
 
 }
