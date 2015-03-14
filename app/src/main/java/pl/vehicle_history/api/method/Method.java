517f6d41949c562c73ee6d74dbda96f76f73ab74
Piotr Makowski <piotr.makowski@allegrogroup.com>
2015-03-14 12:56:04 +0100

    Add unsafe ok http client accepting all certs.

--- app/src/main/java/pl/vehicle_history/api/method/Method.java
+++ app/src/main/java/pl/vehicle_history/api/method/Method.java
@@ -4,10 +4,12 @@ import android.util.Base64;
 
 import java.io.UnsupportedEncodingException;
 
+import pl.vehicle_history.api.UnsafeOkHttpClientProvider;
 import pl.vehicle_history.api.consts.Credentials;
 import pl.vehicle_history.api.consts.Settings;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
 
 /**
  * Created by dudvar on 2015-03-13.
@@ -31,9 +33,11 @@ public abstract class Method<T> {
 
     protected void initializeAdapter() {
         restAdapter = new RestAdapter.Builder()
+                .setClient(new OkClient(new UnsafeOkHttpClientProvider().getUnsafeOkHttpClient()))
                 .setEndpoint(Settings.OAUTH_HOST)
                 .setRequestInterceptor(requestInterceptor)
                 .build();
+
         apiService = restAdapter.create(VehicleHistoryApiInterface.class);
     }
 
