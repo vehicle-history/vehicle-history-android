a930f49d3321135e1bc7a200028396615d61fe0d
m4lysh <m4lysh@gmail.com>
2015-03-19 22:47:30 +0100

    Load credentials from build config.

--- app/src/main/java/pl/vehicle_history/api/consts/Credentials.java
+++ app/src/main/java/pl/vehicle_history/api/consts/Credentials.java
@@ -1,11 +1,13 @@
 package pl.vehicle_history.api.consts;
 
+import pl.vehicle_history.historiapojazdu.BuildConfig;
+
 /**
  * Created by dudvar on 2015-03-13.
  */
 public class Credentials {
-    public static final String LOGIN = "";
-    public static final String PASSWORD = "";
-    public static final String CLIENT = "";
-    public static final String CLIENT_PASSWORD = "";
+    public static final String LOGIN = BuildConfig.API_LOGIN;
+    public static final String PASSWORD = BuildConfig.API_PASSWORD;
+    public static final String CLIENT = BuildConfig.API_CLIENT;
+    public static final String CLIENT_PASSWORD = BuildConfig.API_CLIENT_PASSWORD;
 }
