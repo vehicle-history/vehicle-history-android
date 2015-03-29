02debce834ec98e348dd643be1840a97b6512960
Piotr Makowski <piotr.makowski@allegrogroup.com>
2015-03-29 09:56:03 +0200

    Add HistoryDbOpenHelper.

---
+++ app/src/main/java/pl/vehicle_history/database/DbConstants.java
@@ -0,0 +1,13 @@
+package pl.vehicle_history.database;
+
+public class DbConstants {
+
+    public static final String DATABASE_NAME = "search_history_db";
+    public static final String TABLE_NAME = "history";
+
+    public static final String COLUMN_LABEL = "lalbe";
+    public static final String COLUMN_REGISTRATION_NUMBER = "registration_number";
+    public static final String COLUMN_VIN = "vin";
+    public static final String COLUMN_REGISTRATION_DATE = "registration_date";
+
+}
