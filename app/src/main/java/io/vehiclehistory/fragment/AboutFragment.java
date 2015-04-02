6a53a775169d54d7c0899858739c7c5b2a262ddd
dud <dudvar@o2.pl>
2015-04-02 23:51:36 +0200

    Added app version to about fragment.

--- app/src/main/java/io/vehiclehistory/fragment/AboutFragment.java
+++ app/src/main/java/io/vehiclehistory/fragment/AboutFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import io.vehiclehistory.BuildConfig;
 import io.vehiclehistory.R;
 import io.vehiclehistory.activity.MainActivity;
 
@@ -32,6 +34,10 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView applicationVersion = (TextView) rootView.findViewById(R.id.application_version);
+        applicationVersion.setText(getResources().getString(R.string.about_application_version) + " " + BuildConfig.VERSION_NAME);
+        
         return rootView;
     }
 
