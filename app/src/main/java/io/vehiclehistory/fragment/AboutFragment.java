2a45f9ff39b4e86ff521dbb246ab51a91f784917
m4lysh <m4lysh@gmail.com>
2015-04-03 18:28:20 +0200

    About layout improvements.

--- app/src/main/java/io/vehiclehistory/fragment/AboutFragment.java
+++ app/src/main/java/io/vehiclehistory/fragment/AboutFragment.java
@@ -1,6 +1,7 @@
 package io.vehiclehistory.fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import io.vehiclehistory.activity.MainActivity;
 public class AboutFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
+    public static final String MAILTO_URI_PART = "mailto";
 
     private TextView applicationVersionTextView;
     private Button contactButton;
@@ -58,8 +60,8 @@ public class AboutFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto", "developer@vehiclehistory.io", null));
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Historia pojazdu");
+                        MAILTO_URI_PART, getActivity().getString(R.string.contact_email), null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, getActivity().getString(R.string.app_name));
                 startActivity(Intent.createChooser(emailIntent, getResources().getString(R.string.about_contact)));
             }
         });
