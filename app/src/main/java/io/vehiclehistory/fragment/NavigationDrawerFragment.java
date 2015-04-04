eb4e2c4df2efb12d88f64c19a6def1ed505815f8
m4lysh <m4lysh@gmail.com>
2015-04-04 22:46:12 +0200

    Use v7 ActionBarDrawerToggle instead of v4.

--- app/src/main/java/io/vehiclehistory/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/io/vehiclehistory/fragment/NavigationDrawerFragment.java
@@ -6,11 +6,11 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -132,7 +132,6 @@ public class NavigationDrawerFragment extends Fragment {
         drawerToggle = new ActionBarDrawerToggle(
                 getActivity(),                    /* host Activity */
                 NavigationDrawerFragment.this.drawerLayout,                    /* DrawerLayout object */
-                R.drawable.ic_drawer,             /* nav drawer image to replace 'Up' caret */
                 R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
                 R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
         ) {
