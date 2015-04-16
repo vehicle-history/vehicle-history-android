a694adeb1e46890437a3618aedab3e280c01999e
m4lysh <m4lysh@gmail.com>
2015-04-16 21:54:05 +0200

    Fix SS1.

--- app/src/main/java/io/vehiclehistory/SaveSearchDelegate.java
+++ app/src/main/java/io/vehiclehistory/SaveSearchDelegate.java
@@ -2,16 +2,19 @@ package io.vehiclehistory;
 
 import android.content.Context;
 
+import io.vehiclehistory.api.OptionalProvider;
 import io.vehiclehistory.api.model.VehicleInput;
 import io.vehiclehistory.api.model.VehicleResponse;
 import io.vehiclehistory.database.SearchHistoryDb;
 
 public class SaveSearchDelegate {
 
-    SearchHistoryDb searchHistoryDb;
+    private final SearchHistoryDb searchHistoryDb;
+    private final OptionalProvider optionalProvider;
 
     public SaveSearchDelegate(Context context) {
         searchHistoryDb = new SearchHistoryDb(context);
+        optionalProvider = new OptionalProvider(context);
     }
 
     public void saveSearch(VehicleInput input, VehicleResponse response) {
@@ -20,6 +23,6 @@ public class SaveSearchDelegate {
     }
 
     private String buildLabel(VehicleResponse response) {
-        return response.getVehicle().getName().getMake().toString() + " " + response.getVehicle().getName().getModel();
+        return optionalProvider.getMakePlusModel(response.getVehicle());
     }
 }
